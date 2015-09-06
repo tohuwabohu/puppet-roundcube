a05189be8189b5e5b873ea8d606e80fe703495ae
Martin Meinhold <Martin.Meinhold@gmx.de>
2015-09-06 17:15:03 +1000

    Revert "#2 Downgrade pear/net_smtp to 1.6.2"
    This reverts commit 00c2c40826fff60b01ac88cc89877ba435a2b820.

--- manifests/workarounds/conflicting_pear_dependency.pp
+++ manifests/workarounds/conflicting_pear_dependency.pp
@@ -1,8 +1,8 @@
 # == Class: roundcube::workarounds::conflicting_pear_dependency
 #
 # Both - `pear/net_smtp` and `pear/mail_mime` depend in more recent versions on `pear/pear-core-minimal` which
-# conflicts with the `pear-pear.php.net/PEAR` dependency. Installing the same version of the modules that are shipped
-# in the complete archive available on sourceforge. This ensures `pear-core-minimal` is not pulled in.
+# conflicts with the `pear-pear.php.net/PEAR` dependency. Installing older version of the modules to ensure
+# `pear-core-minimal` is not pulled in.
 #
 #
 class roundcube::workarounds::conflicting_pear_dependency {
@@ -12,27 +12,16 @@ class roundcube::workarounds::conflicting_pear_dependency {
 
   $application_dir = $roundcube::install::target
   $composer_net_smtp_package_name = 'pear/net_smtp'
-  $composer_net_smtp_package_version = '1.6.2'
+  $composer_net_smtp_package_version = '1.6.3'
   $composer_mail_mime_package_name = 'pear/mail_mime'
   $composer_mail_mime_package_version = '1.8.9'
-
-  augeas { "${application_dir}/composer.json__repositories_Net_SMTP":
-    lens    => 'Json.lns',
-    incl    => "${application_dir}/composer.json",
-    changes => [
-      "set dict/entry[. = 'repositories']/array/dict[4]/entry[2]/dict/entry[. = 'version']/string ${composer_net_smtp_package_version}",
-      "set dict/entry[. = 'repositories']/array/dict[4]/entry[2]/dict/entry[. = 'name']/string ${composer_net_smtp_package_name}",
-      "set dict/entry[. = 'repositories']/array/dict[4]/entry[2]/dict/entry[. = 'source']/dict/entry[. = 'reference']/string ${composer_net_smtp_package_version}",
-    ],
-    require => Class['roundcube::install'],
-  }
 
   exec { "${roundcube::composer_command_name} require ${composer_net_smtp_package_name}:${composer_net_smtp_package_version} --update-no-dev --update-with-dependencies":
     unless      => "${roundcube::composer_command_name} show --installed ${composer_net_smtp_package_name} ${composer_net_smtp_package_version}",
     cwd         => $application_dir,
     path        => $roundcube::exec_paths,
     environment => $roundcube::composer_exec_environment,
-    require     => Augeas["${application_dir}/composer.json__repositories_Net_SMTP"],
+    require     => Class['roundcube::install'],
   }
 
   exec { "${roundcube::composer_command_name} require ${composer_mail_mime_package_name}:${composer_mail_mime_package_version} --update-no-dev --update-with-dependencies":
