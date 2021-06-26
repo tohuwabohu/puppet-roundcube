92ab412ee52f48b9bd9255b96f5857f57e85e1c5
Martin Meinhold <Martin.Meinhold@gmx.de>
2021-06-26 22:35:10 +0200

    Replace deprecated markasjunk2 with carddav plugin

--- spec/acceptance/plugin_spec.rb
+++ spec/acceptance/plugin_spec.rb
@@ -143,8 +143,8 @@ describe 'roundcube::plugin' do
         }
 
         class { 'roundcube': }
-        roundcube::plugin { 'johndoh/markasjunk2':
-          ensure => 'dev-release-1.9',
+        roundcube::plugin { 'roundcube/carddav':
+          ensure => 'v4.1.2',
         }
       EOS
     }
@@ -157,11 +157,11 @@ describe 'roundcube::plugin' do
       apply_manifest(manifest, :catch_changes => true)
     end
 
-    describe file('/var/www/roundcubemail/plugins/markasjunk2') do
+    describe file('/var/www/roundcubemail/plugins/carddav') do
       it { should be_directory }
     end
 
-    describe file('/var/www/roundcubemail/plugins/markasjunk2/config.inc.php') do
+    describe file('/var/www/roundcubemail/plugins/carddav/config.inc.php') do
       it { should be_file }
       it { should be_owned_by 'www-data' }
       it { should be_grouped_into 'www-data' }
@@ -169,120 +169,7 @@ describe 'roundcube::plugin' do
     end
 
     describe file('/var/www/roundcubemail/config/config.inc.php') do
-      its(:content) { should match /^\s*'markasjunk2',$/ }
-    end
-
-    describe command('php -l /var/www/roundcubemail/config/config.inc.php') do
-      its(:stdout) { should match /No syntax errors/ }
-    end
-  end
-
-  context 'plugin from repository with configuration parameters' do
-    let(:manifest) { <<-EOS
-        $required_directories = [
-          '/opt',
-          '/var/cache/puppet',
-          '/var/cache/puppet/archives',
-          '/var/www',
-        ]
-
-        file { $required_directories:
-          ensure => directory,
-        }
-
-        class { 'roundcube': }
-        roundcube::plugin { 'johndoh/markasjunk2':
-          ensure       => 'dev-release-1.9',
-          options_hash => {
-            'markasjunk2_read_spam' => false,
-          },
-        }
-      EOS
-    }
-
-    specify 'should provision with no errors' do
-      apply_manifest(manifest, :catch_failures => true)
-    end
-
-    specify 'should be idempotent' do
-      apply_manifest(manifest, :catch_changes => true)
-    end
-
-    describe file('/var/www/roundcubemail/plugins/markasjunk2/config.inc.php') do
-      its(:content) { should match /^\$config\['markasjunk2_read_spam'\] = false;$/ }
-    end
-
-    describe command('php -l /var/www/roundcubemail/config/config.inc.php') do
-      its(:stdout) { should match /No syntax errors/ }
-    end
-  end
-
-  context 'plugin from repository with custom template' do
-    let(:manifest) { <<-EOS
-        $required_directories = [
-          '/opt',
-          '/var/cache/puppet',
-          '/var/cache/puppet/archives',
-          '/var/www',
-        ]
-
-        file { $required_directories:
-          ensure => directory,
-        }
-
-        class { 'roundcube': }
-        roundcube::plugin { 'johndoh/markasjunk2':
-          ensure               => 'dev-release-1.9',
-          config_file_template => 'roundcube/config/options.php.erb',
-          options_hash         => {
-            'markasjunk2_unread_ham' => true,
-          },
-        }
-      EOS
-    }
-
-    specify 'should provision with no errors' do
-      apply_manifest(manifest, :catch_failures => true)
-    end
-
-    specify 'should be idempotent' do
-      apply_manifest(manifest, :catch_changes => true)
-    end
-
-    describe file('/var/www/roundcubemail/plugins/markasjunk2/config.inc.php') do
-      its(:content) { should match /^\$config\['markasjunk2_unread_ham'\] = true;$/ }
-    end
-
-    describe command('php -l /var/www/roundcubemail/config/config.inc.php') do
-      its(:stdout) { should match /No syntax errors/ }
-    end
-  end
-
-  context 'plugin without default configuration' do
-    let(:manifest) { <<-EOS
-        $required_directories = [
-          '/opt',
-          '/var/cache/puppet',
-          '/var/cache/puppet/archives',
-          '/var/www',
-        ]
-
-        file { $required_directories:
-          ensure => directory,
-        }
-
-        class { 'roundcube':
-          plugins => [ 'emoticons' ],
-        }
-      EOS
-    }
-
-    specify 'should provision with no errors' do
-      apply_manifest(manifest, :catch_failures => true)
-    end
-
-    specify 'should be idempotent' do
-      apply_manifest(manifest, :catch_changes => true)
+      its(:content) { should match /^\s*'carddav',$/ }
     end
 
     describe command('php -l /var/www/roundcubemail/config/config.inc.php') do
