001a6f83c99cd5690bb09e0f38eea06f6b251c66
Martin Meinhold <Martin.Meinhold@gmx.de>
2024-02-18 15:50:09 +0100

    Update Puppet dependencies

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -6,12 +6,12 @@ require 'beaker/module_install_helper'
 # collection: puppet - latest, puppet6 is the current version
 install_puppet_agent_on(hosts, {:puppet_collection => 'puppet' } )
 install_module_on(hosts)
-install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '= 6.6.0')       # 7.x blocked by composer
-install_module_from_forge_on(hosts, 'puppetlabs-concat', '= 8.0.1')       # 9.x blocked by stdlib
+install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '= 9.4.1')       # 7.x blocked by composer
+install_module_from_forge_on(hosts, 'puppetlabs-concat', '= 9.0.2')       # 9.x blocked by stdlib
 install_module_from_forge_on(hosts, 'willdurand-composer', '= 1.2.10')
 install_module_from_forge_on(hosts, 'camptocamp-archive', '= 0.8.1')
 # test dependencies
-install_module_from_forge_on(hosts, 'puppetlabs-apache', '= 10.1.0')
+install_module_from_forge_on(hosts, 'puppetlabs-apache', '= 12.0.2')
 
 RSpec.configure do |c|
   c.formatter = :documentation
